31b1d3c42c8851b54d7faaabfa16510a145de0f9
hamabarznji <hama.aak1@gmail.com>
2023-07-22 00:03:20 +0300

    seperated functions

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-import expenses from "../seedData/expenses";
+import seedExpenses from "../seedData/expenses";
 import users from "../seedData/users";
-import expenseCategories from "../seedData/expenseCategories";
-
+import seedExpenseCategories from "../seedData/expenseCategories";
+import itemCategories from "../seedData/itemCategories";
+import seedItemCategories from "../seedData/vendors";
 class Seeder {
   async seedUser() {
     try {
@@ -18,38 +19,28 @@ class Seeder {
     }
   }
 
-  async seedExpenseCategories() {
-    try {
-      const categories = await prisma.expenseCategory.findMany();
-      if (categories.length === 0) {
-        await prisma.expenseCategory.createMany({
-          data: expenseCategories,
-        });
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
 
-  async seedExpenses() {
-    try {
-      const existingExpenses = await prisma.expense.findMany();
-      if (existingExpenses.length === 0) {
-        await prisma.expense.createMany({
-          data: expenses,
-        });
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
+
+  // async seedItemCategories() {
+  //   try {
+  //     const existingItemCategory = await prisma.itemCategory.findMany();
+  //     if (existingItemCategory.length === 0) {
+  //       await prisma.itemCategory.createMany({
+  //         data: itemCategories,
+  //       });
+  //     }
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
 
   async seedData() {
     try {
-      await prisma.$transaction(async (prisma) => {
+      await prisma.$transaction(async () => {
         await this.seedUser();
-        await this.seedExpenseCategories();
-        await this.seedExpenses();
+        await seedExpenseCategories(prisma);
+        await seedExpenses(prisma);
+        await seedItemCategories(prisma)
       });
     } catch (error) {
       throw error;
