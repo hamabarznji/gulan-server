b0f55b4e8a8c4fd8d58355143ee7f677fb98798e
hamabarznji <hama.aak1@gmail.com>
2023-07-22 00:06:19 +0300

    added vendor data

--- seedData/vendors.ts
+++ seedData/vendors.ts
@@ -1,21 +1,24 @@
 const vendors = [
-    { name: 'Vendor 1' },
-    { name: 'Vendor 2' },
-    { name: 'Vendor 3' },
-    { name: 'Vendor 4' },
-    { name: 'Vendor 5' },
-  ];
+  {
+    id: "e41679c5-cdf4-420e-92c2-1b2685551092",
+    name: 'Vendor 1'
+  },
+  { id: "ad862761-e006-42bd-a183-3558c3d79d3f", name: 'Vendor 2' },
+  { id: "7b35693c-9b88-4a98-b244-158c461b1099", name: 'Vendor 3' },
+  { id: "4477c860-21c7-4074-b52f-d5fdaaf96760", name: 'Vendor 4' },
+  { id: "30d6a2d4-d524-4aa1-8345-83257aa05801", name: 'Vendor 5' },
+];
 
- const  seedItemCategories=async(prisma)=> {
-    try {
-      const existingItemCategory = await prisma.itemCategory.findMany();
-      if (existingItemCategory.length === 0) {
-        await prisma.itemCategory.createMany({
-          data: vendors,
-        });
-      }
-    } catch (error) {
-      throw error;
+const seedVendors = async (prisma) => {
+  try {
+    const existingItemCategory = await prisma.vendor.findMany();
+    if (existingItemCategory.length === 0) {
+      await prisma.vendor.createMany({
+        data: vendors,
+      });
     }
+  } catch (error) {
+    throw error;
   }
-  export default seedItemCategories
+}
+export default seedVendors
