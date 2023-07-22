6226c05793882f544be463f8dfb4c0494b682b53
hamabarznji <hama.aak1@gmail.com>
2023-07-22 22:11:45 +0300

    updated cat ids

--- seedData/items.ts
+++ seedData/items.ts
@@ -17,7 +17,7 @@ const items = [
   },
   {
     "id": "577cac06-3f19-4342-94ac-7d6f423aa6e2",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "29f8b5d4-8b39-4364-9843-b47fecff7032",
     "name": "ITEM 3",
     "code": "JN789",
     "color_id": "e3330405-e895-42ce-9441-d837a49619ba",
@@ -26,7 +26,7 @@ const items = [
   },
   {
     "id": "6ff4b9aa-c05c-4502-b732-300a672248cd",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "29f8b5d4-8b39-4364-9843-b47fecff7032",
     "name": "ITEM 4",
     "code": "TR987",
     "color_id": "e3330405-e895-42ce-9441-d837a49619ba",
@@ -34,7 +34,7 @@ const items = [
   },
   {
     "id": "7faff783-6879-4ac1-83a1-79a6c100b3e3",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "2aaad8ad-f801-408c-ad2c-6be51dd73b38",
     "name": "ITEM 5",
     "code": "FS567",
     "color_id": "ab2936f9-58a3-4c6d-9da6-f2f06cbe6414",
@@ -42,7 +42,7 @@ const items = [
   },
   {
     "id": "ae05bbc6-56e6-4454-9a49-448a6b5ef02c",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "2aaad8ad-f801-408c-ad2c-6be51dd73b38",
     "name": "ITEM 6",
     "code": "WS777",
     "color_id": "ab2936f9-58a3-4c6d-9da6-f2f06cbe6414",
@@ -50,7 +50,7 @@ const items = [
   },
   {
     "id": "b927b4a3-9fa6-4c12-93c8-03d566c9816e",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "3ec95c3e-d53a-4261-a31b-98d3c3c15e9b",
     "name": "ITEM 7",
     "code": "CS234",
 
@@ -59,7 +59,7 @@ const items = [
   },
   {
     "id": "bb6366d6-8f97-48c5-ab99-225d56e463bc",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "3ec95c3e-d53a-4261-a31b-98d3c3c15e9b",
     "name": "ITEM 8",
     "code": "SJ111",
     "color_id": "ab15b026-7c3d-49be-9c4b-f7f4463f812c",
@@ -67,7 +67,7 @@ const items = [
   },
   {
     "id": "ce1099d8-e5ca-456b-91a7-2bed48c82b42",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "44061b37-5510-4bdd-815e-06e8c5ec7b18",
     "name": "ITEM 9",
     "code": "CP222",
     "color_id": "05a23361-81ff-48ed-96f1-5294b97a1653",
@@ -75,7 +75,7 @@ const items = [
   },
   {
     "id": "cf510d8a-f982-4bc6-bb85-ae415f8df39d",
-    "category_id": "07419e4e-f966-4d13-9b74-e2f7297b61fe",
+    "category_id": "44061b37-5510-4bdd-815e-06e8c5ec7b18",
     "name": "ITEM 10",
     "code": "PB789",
     "color_id": "05a23361-81ff-48ed-96f1-5294b97a1653",
