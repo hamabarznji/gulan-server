4bc348ab729e33a4c96244f3756d8d7eb003dd0b
hamabarznji <hama.aak1@gmail.com>
2023-10-20 00:22:49 +0300

    updated auth

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -6,9 +6,9 @@ import passport from "../../middleware/AuthMiddleware";
 const router = Router();
 
 router.post(routes.user.login, UserController.login);
-router.post(routes.user.createUser, UserController.createUser);
 
 router.use(passport.authenticate("jwt", { session: false }));
+router.post(routes.user.createUser, UserController.createUser);
 
 router.patch(routes.user.updateUser, UserController.updateUser);
 router.get(routes.user.getUser, UserController.getUser);
