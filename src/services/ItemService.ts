5964b410a04f9f61525d091cb4e1f7b5df31373a
hamabarznji <hama.aak1@gmail.com>
2023-10-28 02:46:48 +0300

    added oredredItems

--- src/services/ItemService.ts
+++ src/services/ItemService.ts
@@ -1,13 +1,16 @@
 import prisma from "../../PrismaInstance";
-import { Item, Prisma, purchasedItem } from "@prisma/client";
+import { Item, OrderedItem, Prisma, purchasedItem } from "@prisma/client";
 import ItemCategoryService from "./ItemCategoryService";
 import ItemColorService from "./ItemColorService";
 import ItemSizeService from "./ItemSizeService";
+import SellOrderService from "./SellOrderService";
 class ItemService {
 
 
-    async getItems(): Promise<{ items: Item[], purchasedItems: purchasedItem[] }> {
+    async getItems(): Promise<{ items: Item[], purchasedItems: purchasedItem[],orderedItems:any }> {
         try {
+
+            const orderedItems=await SellOrderService.getOrderedItems();
             const items = await prisma.item.findMany({
                 include: {
                     color: true,
@@ -15,8 +18,16 @@ class ItemService {
                     category: true,
                 }
             });
-            const purchasedItems = await prisma.purchasedItem.findMany({
 
+        //     const rawQuery = await prisma.$queryRaw`
+        //     SELECT * FROM item
+        //     LEFT JOIN color ON item.color_id = color.id
+        //     LEFT JOIN size ON item.size_id = size.id
+        //     LEFT JOIN item_category ON item.category_id = item_category.id
+        //     LEFT JOIN ordered_item ON item.id = ordered_item.item_id
+        // `;
+        
+            const purchasedItems = await prisma.purchasedItem.findMany({
                 include: {
                     item: {
                         include: {
@@ -29,7 +40,9 @@ class ItemService {
             })
             return {
                 items,
-                purchasedItems: purchasedItems
+                purchasedItems: purchasedItems,
+                orderedItems
+                
             }
         } catch (error) {
             console.error('Error retrieving items:', error);
