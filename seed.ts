83c98166048462f21e9f3fdfb9d989e690fb00fc
hamabarznji <hama.aak1@gmail.com>
2023-11-04 04:37:03 +0300

    update files

--- seed.ts
+++ seed.ts
@@ -4,7 +4,11 @@ import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 
 const users: User[] = [
+  // @ts-ignore
+
   { id: uuidv4(), name: 'Gullan', email: 'gullan@example.com', password:bcrypt.hashSync('password1', 10) , role: 'ADMIN', themeColor: true },
+// @ts-ignore
+
   { id: uuidv4(), name: 'User', email: 'user@example.com', password: bcrypt.hashSync('password1', 10), role: 'USER', themeColor: true },
 ];
 
